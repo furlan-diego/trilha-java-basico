42ced8ade83bade75daaeb9e915f239690fba3a0
furlan-diego <furlanaugustodiego@gmail.com>
2025-07-09 20:57:23 -0300

    simulando uma operação de saque

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double saldo = 25.0;
+        double valorSolicitado = 18.0;
+
+        System.out.println("--- Primeio Cenario --- ");
+        System.out.println("Saldo Inicial: " + saldo);
+        System.out.println("valor Solicitado: " + valorSolicitado);
+
+        if(valorSolicitado <= saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+
+        System.out.println("Saldo Atual: " + saldo);
     }
 }
