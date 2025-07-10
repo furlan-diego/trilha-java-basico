750ac26c86fa7d565e26e4c4d256bd5e0c81c210
furlan-diego <furlanaugustodiego@gmail.com>
2025-07-09 21:05:50 -0300

    simulacao de saque com saldo insuficiente

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -3,17 +3,21 @@ public class ContaTerminal {
         double saldo = 25.0;
         double valorSolicitado = 18.0;
 
-        System.out.println("--- Primeio Cenario --- ");
-        System.out.println("Saldo Inicial: " + saldo);
-        System.out.println("valor Solicitado: " + valorSolicitado);
+        // Segundo cenário de teste
+        saldo = 15.0;
+        valorSolicitado = 22.0;
 
-        if(valorSolicitado <= saldo) {
+        System.out.println("--- Segundo Cenário ---");
+        System.out.println("Saldo Inicial: " + saldo);
+        System.out.println("Valor Solicitado: " + valorSolicitado);
+
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-            System.out.println("Saque realizado com sucesso!");
         } else {
             System.out.println("Saldo insuficiente.");
         }
 
-        System.out.println("Saldo Atual: " + saldo);
+        System.out.println("Saldo Final: " + saldo);
+        System.out.println("-----------------------");
     }
 }
